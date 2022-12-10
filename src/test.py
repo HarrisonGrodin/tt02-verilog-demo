c0a7e880366c9ebd62c8832e40b212235f2170a0
Harrison Grodin <hgrodin@andrew.cmu.edu>
2022-12-09 21:06:29 -0500

    Add collatz

--- src/test.py
+++ src/test.py
@@ -3,21 +3,23 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 
-segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
+numbers = list(range(2**3))
+
 
 @cocotb.test()
-async def test_7seg(dut):
-    dut._log.info("start")
-    clock = Clock(dut.clk, 10, units="us")
-    cocotb.start_soon(clock.start())
+async def test_collatz(dut):
+    assert True
+    # dut._log.info("start")
+    # clock = Clock(dut.clk, 10, units="us")
+    # cocotb.start_soon(clock.start())
 
-    dut._log.info("reset")
-    dut.rst.value = 1
-    await ClockCycles(dut.clk, 10)
-    dut.rst.value = 0
+    # dut._log.info("reset")
+    # dut.rst.value = 1
+    # await ClockCycles(dut.clk, 10)
+    # dut.rst.value = 0
 
-    dut._log.info("check all segments")
-    for i in range(10):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, 100)
-        assert int(dut.segments.value) == segments[i]
+    # dut._log.info("check all segments")
+    # for i in range(10):
+    #     dut._log.info("check segment {}".format(i))
+    #     await ClockCycles(dut.clk, 100)
+    #     assert int(dut.segments.value) == segments[i]
